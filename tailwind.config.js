255f2fdaf3957b4cbc50c61111077e70882aaac8
Narawit Tubtimtoe <johndoe@domain.com>
2021-09-15 21:49:14 +0700

    add calculator

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "monitor-light": "#ffe53d",
+        "monitor-dark": "#ffb13d",
+      },
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
